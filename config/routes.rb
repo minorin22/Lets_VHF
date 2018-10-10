947a1936da970bc901060bb2849ea81edd894ed8
Minorin <minorin@studio-mizutama.net>
2018-10-10 17:58:03 +0900

    create users by scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   get "/" => "home#top"
-  get "about" => "home#about" 
+  get "about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users" => "users#index"
+  get "users/:id" => "users#show"
+  get "signup" => "users#new"
+  post "users" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  put "users/:id" => "users#update"
+  delete "users/:id" => "users#destroy"
 end
