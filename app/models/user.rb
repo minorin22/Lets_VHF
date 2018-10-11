d2a29d923dd4c4a47ce136ff05dc51ff9b42e727
nekonekoBook <nekonekoBook@nekonekoBook.local>
2018-10-11 22:31:36 +0900

    Start bitbucket

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password validations: true
+  validates :name, {presence: true, uniqueness: true, length: {maximum: 20}, confirmation: true}
+  validates :password, {presence: true, length: {maximum: 20}}
 end
