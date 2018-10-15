6b05ba3adf74a2790a5de813800db93a244877e8
Minorin <tekotekowalknyan@icloud.com>
2018-10-15 19:01:20 +0900

    Create Station Model and Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
+    if @current_user
+      @current_station = Station.find_by(user_id: @current_user.id)
+    end
   end
 end
