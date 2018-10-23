985c2073152f9e571178777c81cbef7f47dd68b7
Minorin <tekotekowalknyan@icloud.com>
2018-10-23 19:27:39 +0900

    power and chancel btn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_005446) do
+ActiveRecord::Schema.define(version: 2018_10_23_004751) do
 
   create_table "stations", force: :cascade do |t|
     t.integer "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_22_005446) do
     t.datetime "updated_at", null: false
     t.integer "state"
     t.integer "tmp_ch"
+    t.integer "power"
   end
 
   create_table "users", force: :cascade do |t|
