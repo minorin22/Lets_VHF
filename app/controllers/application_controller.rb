43d99888a0396fab12b2a4b682f651c7aa3afb01
nekonekoBook <nekonekoBook@nekonekoBook.local>
2018-11-05 19:14:48 +0900

    Create_dsc_show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
-
+  protect_from_forgery
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
     if @current_user
