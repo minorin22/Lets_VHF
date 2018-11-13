c64d99eab55f5aa3dc7615e0581155321844a715
nekonekoBook <nekonekoBook@nekonekoBook.local>
2018-11-13 19:31:38 +0900

    create urgency call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,32 @@ class ApplicationController < ActionController::Base
       @current_station = Station.find_by(user_id: @current_user.id)
     end
   end
+
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = "ログインが必要です"
+      redirect_to("/login")
+    end
+  end
+
+  def forbid_login_user
+    if @current_user
+      flash[:notice] = "すでにログインしています"
+      redirect_to("/users/#{@current_user.id}")
+    end
+  end
+
+  def authenticate_station
+    if @current_station == nil
+      flash[:notice] = "局情報の登録が必要です"
+      redirect_to("/stations/new")
+    end
+  end
+
+  def forbid_login_station
+    if @current_station
+      flash[:notice] = "すでに登録しています"
+      redirect_to("/stations/#{@current_station.id}")
+    end
+  end
 end
