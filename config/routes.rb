e867ee504b2cf47d9c92fc9135f77dbc308d4736
Minorin <minorin@studio-mizutama.net>
2018-11-26 18:06:16 +0900

    create ais call

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   post "stations/:id/proxy_distress_call_all_ships" => "stations#proxy_distress_call_all_ships"
   post "stations/:id/dsc_test_call" => "stations#dsc_test_call"
   post "stations/:id/other_ships_list" => "stations#other_ships_list"
+  post "stations/:id/ais_call" => "stations#ais_call"
   delete "stations/:id" => "stations#destroy"
 
   post "dscs/ship_station_call" => "dscs#ship_station_call"
