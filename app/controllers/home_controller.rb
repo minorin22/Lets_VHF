70609bbcd789ed95c846732f35d3efea60d1d931
nekonekoBook <nekonekoBook@nekonekoBook.local>
2018-12-04 19:16:14 +0900

    create favicon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
+  before_action :forbid_login_user_home, only: [:top, :about]
   def top
   end
   def about
+  end
+  def forbid_login_user_home
+    if @current_user
+      redirect_to("/users/#{@current_user.id}")
+    end
   end
 end
