8ce0fcfcf3f6242f78b6a89775358ac0a24fc50c
nekonekoBook <nekonekoBook@nekonekoBook.local>
2018-12-12 15:05:10 +0900

    continue login status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,20 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
   protect_from_forgery
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+    #@current_user = User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user_id = cookies.signed[:user_id]
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+      #if user && BCrypt::Password.new(user.remember_digest).is_password?(cookies[:remember_token])
+        session[:user_id] = user.id
+        @current_user = user
+      end
+    end
+
+
     if @current_user
       @current_station = Station.find_by(user_id: @current_user.id)
     end
